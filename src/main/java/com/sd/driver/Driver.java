bc32bffa7cb3e53a13d76f0fb304fdf780532ecc
dassoumalyadas <dassoumalyadas@gmail.com>
2023-03-06 13:47:46 +0530

    Implemented Web Mobile switch.

--- src/main/java/com/sd/driver/Driver.java
+++ src/main/java/com/sd/driver/Driver.java
@@ -7,29 +7,36 @@ import com.sd.driver.manager.DriverThreadManager;
 import com.sd.enums.MobilePlatformType;
 import org.openqa.selenium.WebDriver;
 
+import java.util.Objects;
+
 import static com.sd.config.Factory.ConfigFactory.getConfig;
 
-public class Driver {
-    private Driver(){}
-    private static WebDriver driver;
+public final class Driver {
+    private Driver() {
+    }
 
     //Web- Local,SeleniumGrid,Selenoid,Browserstack
     //Mobile - local,Browserstack
     public static void initDriverWeb() {
-        WebDriverData driverData = new WebDriverData(getConfig().browser(), getConfig().browserRemoteMode());
-        driver = DriverFactory.getDriverForWeb(getConfig().browserRunMode()).getDriver(driverData);
-        DriverThreadManager.setDriver(driver);
-
+        if (Objects.isNull(DriverThreadManager.getDriver())) {
+            WebDriverData driverData = new WebDriverData(getConfig().browser(), getConfig().browserRemoteMode());
+            WebDriver driver = DriverFactory.getDriverForWeb(getConfig().browserRunMode()).getDriver(driverData);
+            DriverThreadManager.setDriver(driver);
+        }
     }
 
     public static void initDriverMobile() {
-        MobileDriverData driverData = new MobileDriverData(MobilePlatformType.ANDROID, getConfig().mobileRemoteMode());
-        driver = DriverFactory.getDriverForMobile(getConfig().mobileRunMode()).getDriver(driverData);
-        DriverThreadManager.setDriver(driver);
+        if (Objects.isNull(DriverThreadManager.getDriver())) {
+            MobileDriverData driverData = new MobileDriverData(MobilePlatformType.ANDROID, getConfig().mobileRemoteMode());
+            WebDriver driver = DriverFactory.getDriverForMobile(getConfig().mobileRunMode()).getDriver(driverData);
+            DriverThreadManager.setDriver(driver);
+        }
     }
 
     public static void quitDriver() {
-        driver.quit();
-        DriverThreadManager.unload();
+        if (Objects.isNull(DriverThreadManager.getDriver())) {
+            DriverThreadManager.getDriver().quit();
+            DriverThreadManager.unload();
+        }
     }
 }
--- src/main/java/com/sd/driver/manager/DriverThreadManager.java
+++ src/main/java/com/sd/driver/manager/DriverThreadManager.java
@@ -1,23 +1,54 @@
 package com.sd.driver.manager;
 
+import com.sd.enums.PlatformType;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
 import org.openqa.selenium.WebDriver;
 
+import java.util.EnumMap;
+import java.util.Map;
+
+import static com.sd.enums.PlatformType.MOBILE;
+import static com.sd.enums.PlatformType.WEB;
+import static java.lang.ThreadLocal.withInitial;
+
 public final class DriverThreadManager {
+    //Web--> Mobile
+    //Web--> Mobile--> Web
 
     private DriverThreadManager() {
     }
 
-    public static final ThreadLocal<WebDriver> THREAD_LOCAL = new ThreadLocal<>();
+    //List,Map --> WEB,WEBDRIVER_THREAD_LOCAL; MOBILE,MOBILE_DRIVER_THREAD_LOCAL
+    //At a particular instance ,context --> WEB,MOBILE
+    private static final ThreadLocal<WebDriver> WEBDRIVER_THREAD_LOCAL = new ThreadLocal<>();
+    private static final ThreadLocal<WebDriver> MOBILE_DRIVER_THREAD_LOCAL = new ThreadLocal<>();
+    private static final ThreadLocal<PlatformType> CONTEXT = withInitial(() -> WEB);
+    private static final Map<PlatformType, ThreadLocal<WebDriver>> DRIVER_MAP = new EnumMap<PlatformType, ThreadLocal<WebDriver>>(PlatformType.class);
 
     public static WebDriver getDriver() {
-        return THREAD_LOCAL.get();
+        return CONTEXT.get() == WEB ? WEBDRIVER_THREAD_LOCAL.get() : MOBILE_DRIVER_THREAD_LOCAL.get();
+    }
+
+    private static boolean isMobileDriver(WebDriver driver) {
+        return driver instanceof AndroidDriver || driver instanceof IOSDriver;
     }
 
     public static void setDriver(WebDriver driver) {
-        THREAD_LOCAL.set(driver);
+        if (isMobileDriver(driver)) {
+            MOBILE_DRIVER_THREAD_LOCAL.set(driver);
+            CONTEXT.set(MOBILE);
+
+        } else {
+            WEBDRIVER_THREAD_LOCAL.set(driver);
+            DRIVER_MAP.put(WEB, WEBDRIVER_THREAD_LOCAL);
+            CONTEXT.set(WEB);
+        }
     }
 
     public static void unload() {
-        THREAD_LOCAL.remove();
+        WEBDRIVER_THREAD_LOCAL.remove();
+        MOBILE_DRIVER_THREAD_LOCAL.remove();
+        CONTEXT.remove();
     }
 }
